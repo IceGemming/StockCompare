849abbf9d74b9789403f6386d00a36e6f493a23d
IceGemming <94154773+IceGemming@users.noreply.github.com>
2024-06-30 18:13:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,11 @@ if submit:
     today_terminal_value = round(terminal_value / ((1 + discount_rate) ** 5))
     sum_of_future_FCF = sum(discount_rate_4_years_ahead.values()) + today_terminal_value
     enterprise_value = sum_of_future_FCF + cash_eq - debt
-    fair_value = enterprise_value/(ticker.info.get("impliedSharesOutstanding"))
-    # fair_value = 150
+    
+    st.write (ticker.info)
+    
+    # fair_value = enterprise_value/(ticker.info.get("impliedSharesOutstanding"))
+    fair_value = 150
 
     st.table(pd.DataFrame({'Stock Values': {'Fair Value': round(fair_value),
                                             'Current Value': round(ticker.info['previousClose']),
